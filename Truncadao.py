e3127470c53aaa6d6923cbfd07d876f6df4e340c
GabrielBright <gabriel.v@brightisd.com>
2025-06-04 11:28:34 -0300

    Testando novo seletor para revenda e colocando para limpar e formatar o texto coletados

--- Truncadao.py
+++ Truncadao.py
@@ -96,9 +96,12 @@ async def tentar_extrair_dados_tecnicos(pagina, config: Config) -> Dict[str, str
 
 async def tentar_extrair_revenda(pagina, config: Config) -> str:
     try:
-        selector = "//*[@id='__next']/div[2]/div/div[2]/div[2]/div[1]/span/p"
+        selector = "xpath=//*[@id='__next']/div[2]/div/div[2]/div[2]/div[1]/span"
         await pagina.wait_for_selector(selector, timeout=config.TIMEOUT_PADRAO)
-        return await pagina.locator(selector).inner_text()
+        raw_text = await pagina.locator(selector).inner_text()
+        revenda_text = raw_text.replace("CIDADE:", "").strip().title()
+        logger.info(f"🏪 Localização da revenda: {revenda_text}")
+        return revenda_text
     except Exception as e:
         logger.warning(f"Erro na revenda: {e}")
         return "Não informado"
